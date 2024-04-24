a3bcd3dd7e1b8952e080d11bfd109eb8ba74d853
Randolio <103154494+Randolio@users.noreply.github.com>
2024-04-25 00:41:21 +0100

    Update cl_knockout.lua

--- cl_knockout.lua
+++ cl_knockout.lua
@@ -1,48 +1,40 @@
-QBCore = exports["qb-core"]:GetCoreObject()
 local knockedOut = false
 
-local function WakeUp(ped)
-    if knockedOut then
-        ClearTimecycleModifier()
-        lib.hideTextUI()
-        knockedOut = false
-        SetEntityInvincible(ped, false)
-    end
+local function wakeUp()
+    if not knockedOut then return end
+    ClearTimecycleModifier()
+    lib.hideTextUI()
+    knockedOut = false
+    SetEntityInvincible(cache.ped, false)
 end
 
-local function KnockedOutLoop(ped)
-    SetTimecycleModifier("hud_def_blur")
-    lib.showTextUI('You are currently knocked out..', {position = "top-center"})
+local function knockedOutLoop()
+    SetTimecycleModifier('hud_def_blur')
+    lib.showTextUI('You are currently knocked out..', {position = 'top-center'})
     CreateThread(function()
         while knockedOut do
             Wait(100)
-            SetPedToRagdoll(ped, 500, 500, 0, 0, 0, 0)
-            ResetPedRagdollTimer(ped)
-            if IsEntityDead(ped) then
-                WakeUp(ped)
+            SetPedToRagdoll(cache.ped, 500, 500, 0, 0, 0, 0)
+            ResetPedRagdollTimer(cache.ped)
+            if IsEntityDead(cache.ped) then
+                wakeUp()
             end
         end
     end)
 end
 
 AddEventHandler('gameEventTriggered', function(event, data)
-    if event == "CEventNetworkEntityDamage" then
-        local victim = NetworkGetPlayerIndexFromPed(data[1])
-        if victim == PlayerId() then
-            ped = PlayerPedId()
-            if IsPedInMeleeCombat(ped) then
-                if HasPedBeenDamagedByWeapon(ped, GetHashKey("WEAPON_UNARMED"), 0) then
-                    if GetEntityHealth(ped) < Config.Health then
-                        if not knockedOut then
-                            knockedOut = true
-                            SetEntityInvincible(ped, true)
-                            KnockedOutLoop(ped)
-                            Wait(Config.KnockoutTime * 1000)
-                            WakeUp(ped)
-                        end
-                    end
-                end
-            end
+    if event ~= "CEventNetworkEntityDamage" then return end
+    
+    local victim = NetworkGetPlayerIndexFromPed(data[1])
+    if victim ~= cache.playerId or not IsPedInMeleeCombat(cache.ped) then return end
+
+    if HasPedBeenDamagedByWeapon(cache.ped, `WEAPON_UNARMED`, 0) then
+        if GetEntityHealth(cache.ped) < Config.Health and not knockedOut then
+            knockedOut = true
+            SetEntityInvincible(cache.ped, true)
+            knockedOutLoop()
+            SetTimeout(Config.KnockoutTime * 1000, wakeUp)
         end
     end
 end)
